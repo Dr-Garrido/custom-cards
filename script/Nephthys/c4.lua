62edf2746ebc2d289a3160f867c7d4c79ad71c90
Samuel <71157606+Dr-Garrido@users.noreply.github.com>
2024-10-25 15:50:01 -0300

    Add files via upload

--- script/Nephthys/c4.lua
+++ script/Nephthys/c4.lua
@@ -1,16 +1,20 @@
 -- Wrath of Nephthys
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Activação de "Nephthys" Spell/Trap Cards da mão durante a Main Phase
+    -- Ativar
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_ACTIVATE)
+    e0:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e0)
+    
+    -- Permitir ativação de "Nephthys" Spell/Trap Cards da mão
     local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetCode(EFFECT_QP_ACT_IN_NTPHAND)
-    e1:SetRange(LOCATION_SZONE)
-    e1:SetTargetRange(LOCATION_HAND,0)
-    e1:SetTarget(s.target)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+    e1:SetCondition(s.handcon)
     c:RegisterEffect(e1)
 
-    -- Uma vez por turno: destrua 1 monstro na mão ou no campo e Special Summon 1 "Nephthys" Spellcaster do Deck
+    -- Uma vez por turno: destruir 1 monstro na mão ou no campo e Special Summon 1 "Nephthys" Spellcaster do Deck
     local e2=Effect.CreateEffect(c)
     e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_IGNITION)
@@ -30,32 +34,30 @@ function s.initial_effect(c)
     e3:SetTarget(s.sptg)
     e3:SetOperation(s.spop)
     c:RegisterEffect(e3)
-    
 end
+s.listed_series={0x11f}  -- Série Nephthys
 
--- e1: Alvo para ativação de "Nephthys" Spell/Trap Cards da mão
-function s.target(e,c)
-    return c:IsSetCard(0x11f) and c:IsType(TYPE_SPELL+TYPE_TRAP)
+-- Condição para ativação da mão: verifique se o jogador controla um card "Nephthys"
+function s.handcon(e)
+    return Duel.IsExistingMatchingCard(function(c) return c:IsSetCard(0x11f) end, e:GetHandlerPlayer(), LOCATION_ONFIELD, 0, 1, nil)
 end
 
 -- e2: Destroi 1 monstro na mão ou no campo e Special Summon 1 "Nephthys" Spellcaster do Deck
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_HAND+LOCATION_MZONE)
 end
 
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsRelateToEffect(e) then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-        local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
-        if #g>0 and Duel.Destroy(g,REASON_EFFECT)~=0 then
-            -- Special Summon 1 "Nephthys" Spellcaster do Deck
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-            local sg=Duel.SelectMatchingCard(tp,function(c) return c:IsSetCard(0x11f) and c:IsType(TYPE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end,tp,LOCATION_DECK,0,1,1,nil)
-            if #sg>0 then
-                Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-            end
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
+    if #g>0 and Duel.Destroy(g,REASON_EFFECT)~=0 then
+        -- Special Summon 1 "Nephthys" Spellcaster do Deck
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local sg=Duel.SelectMatchingCard(tp,function(c) return c:IsSetCard(0x11f) and c:IsType(TYPE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end,tp,LOCATION_DECK,0,1,1,nil)
+        if #sg>0 then
+            Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
         end
     end
 end
@@ -70,7 +72,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 
-function s.spfilter(c)
+function s.spfilter(c,e,tp)
     return c:IsSetCard(0x11f) and c:GetAttack()==2400 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
@@ -80,15 +82,19 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
         -- Destroi esta carta
         Duel.Destroy(c,REASON_EFFECT)
         -- Destroi 1 carta em cada campo
-        local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
-        if #g>0 then
-            Duel.Destroy(g,REASON_EFFECT)
+        local g1=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,0,1,1,nil)
+        local g2=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,0,LOCATION_ONFIELD,1,1,nil)
+        g1:Merge(g2)
+        if #g1>0 then
+            Duel.Destroy(g1,REASON_EFFECT)
         end
         -- Special Summon 1 "Nephthys" com 2400 ATK
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil)
-        if #sg>0 then
-            Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+        if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+            local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+            if #sg>0 then
+                Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+            end
         end
     end
 end
